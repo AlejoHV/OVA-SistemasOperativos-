c31a61098c9f22d7ba02a53d3d7f9410208fac42
Alejandro Hernandez <jose.hernandez01@uceva.edu.co>
2025-04-24 14:25:28 -0500

    Creación ova de sistemas operativos

---
+++ src/main/java/com/example/sistemas/OvaSistemasOperativosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvaSistemasOperativosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvaSistemasOperativosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistemas/OvaSistemasOperativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvaSistemasOperativosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
